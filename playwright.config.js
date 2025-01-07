97b90c710ca1ab93361c057e7e14febe74901fed
Oleksandr-Leonov <oleksandr.s.leonov@sigma.software>
2025-01-07 18:41:07 +0200

    [UPD] reporters

--- playwright.config.js
+++ playwright.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [["html"], ["github"]],
+  reporter: [["html"], ["json", { outputFile: "playwright-report/report.json" }]],
   use: {
     trace: "on-first-retry",
   },
